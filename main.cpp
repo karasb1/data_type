067261df59505d32d43c791b8ca4b0d4de9d241e
karasb1 <bohdankaras456@gmail.com>
2024-04-20 17:12:52 +0300

    Ex 2 was done.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <Windows.h>
 using namespace std;
 
-int main() :
+int main()
 {
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, 0x06);
+	cout << "\"U lukomor\'a dub zelenij," << endl;
+	cout << " Zlataya zep na dupe tom," << endl;
+	cout << " I dnem i nochju kot uchenij" << endl;
+	cout << " vse hodit po cepi krugom\"" << endl;
+	SetConsoleTextAttribute(hConsole, 0x07);
+	return 0;
 }

e5ae96d42ae97d4fb5103584952745abf545bfce
karasb1 <bohdankaras456@gmail.com>
2024-04-20 17:07:05 +0300

    Ex 1 was done.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <Windows.h>
 using namespace std;
 
-int main() :
+int main()
 {
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	cout << "Name       : ";
+	SetConsoleTextAttribute(hConsole, 0x06);
+	cout << "\"The war and the peace\"" << endl;
+	SetConsoleTextAttribute(hConsole, 0x07);
+	cout << "Avtor      :   L.N. Tolstoj" << endl;
+	cout << "Izdatelstvo:  Piter" << endl;
+	cout << "Pages      : ";
+	SetConsoleTextAttribute(hConsole, 0x05);
+	cout << "500" << endl;
+	SetConsoleTextAttribute(hConsole, 0x07);
+	return 0;
 }
